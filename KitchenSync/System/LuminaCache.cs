fd694c4262b10c184a1057cd728168a5ea71183a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-10-16 16:56:38 -0700

    Add Lumina Cache to speed up lookups

---
+++ KitchenSync/System/LuminaCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Lumina.Excel;
+
+namespace KitchenSync.System;
+
+internal class LuminaCache<T> : IDisposable where T : ExcelRow
+{
+    private readonly Dictionary<uint, T> cache = new();
+
+    public LuminaCache()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        
+    }
+
+    public T GetRow(uint id)
+    {
+        if (cache.ContainsKey(id))
+        {
+            return cache[id];
+        }
+        else
+        {
+            cache.Add(id, Service.DataManager.GetExcelSheet<T>()!.GetRow(id)!);
+            return cache[id];
+        }
+    }
+}
